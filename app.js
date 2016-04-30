4b54d727ae32527a6a9c9261ca7b79476a5be26f
Austin Green <austingreenkansas@gmail.com>
2016-04-30 00:33:20 -0500

    Updated package.json

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
